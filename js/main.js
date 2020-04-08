8778ba4a5f1d64762bbeaf6af48856934105381e
Степанов Юрий <sup710@yandex.ru>
2020-04-08 08:31:32 +0300

    Проверка, нужно ли открывающееся меню

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }, 10)
    
 
-    document.getElementsByClassName("header-menu__switcher")[0].addEventListener("click", function () {
-        document.getElementsByClassName("header-menu")[0].classList.toggle("header-menu_active")
-    })
+    // document.getElementsByClassName("header-menu__switcher")[0].addEventListener("click", function () {
+    //     document.getElementsByClassName("header-menu")[0].classList.toggle("header-menu_active")
+    // })
 }, !1);
